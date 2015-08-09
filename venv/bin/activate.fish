8f7e4927264593defe7a00f1cfec32777da4a99a
Abhimanyu Babbar <abhimanyu.babbar@sics.se>
2015-08-09 17:43:09 +0200

    Updated the corrupted venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/babbar/workspace/URLShortener/venv"
+set -gx VIRTUAL_ENV "/home/babbarshaer/workspace/URLShortener/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
